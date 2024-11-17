d0e8f741d709230adf873572ff96e662aec68b60
JoaoVitor49 <joao.pimentel2017@outlook.com>
2024-11-17 16:44:12 -0300

    NovaPesquisa e Acoes

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/screens/Login";
 import NovaConta from "./src/screens/NovaConta";
+import NovaPesquisa from "./src/screens/NovaPesquisa";
+import AcaoPesquisa from "./src/screens/AcaoPesquisa"
 
 const Stack = createStackNavigator()
 
@@ -9,8 +11,17 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-      <Stack.Screen name="NovaConta" component={NovaConta} options={{headerTintColor: '#573FBA', headerTitle: 'Nova Conta', headerStyle: {backgroundColor: '#2B1D62'}, headerTitleStyle: {fontFamily: 'AveriaLibre-Regular', fontSize: 33, color: '#FFFFFF'} }}/>
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name="NovaConta" component={NovaConta} options={{headerTintColor: '#573FBA', headerTitle: 'Nova Conta', headerStyle: {backgroundColor: '#2B1D62'}, headerTitleStyle: {fontFamily: 'AveriaLibre-Regular', fontSize: 33, color: '#FFFFFF'} }}/>
+        <Stack.Screen name='Nova Pesquisa' 
+                        component={NovaPesquisa}
+                        options={{headerStyle:{backgroundColor: '#2B1D62'},
+                        headerTintColor:'#573FBA',
+                        headerTitleStyle:{color: '#FFFFFF', fontFamily: 'AveriaLivre-Regular'}}}/>
+          <Stack.Screen name='Ações' component={AcaoPesquisa}
+                        options={{headerStyle:{backgroundColor: '#2B1D62'},
+                        headerTintColor:'#573FBA',
+                        headerTitleStyle:{color: '#FFFFFF', fontFamily: 'AveriaLivre-Regular'}}}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
